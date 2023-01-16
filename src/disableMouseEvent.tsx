beca2e18996228cf06d1b60bbf709c746766bafb
Michael <michael2ga3@gmail.com>
2023-01-16 14:39:37 +0300

    Add exception error function

--- src/disableMouseEvent.tsx
+++ src/disableMouseEvent.tsx
@@ -1,13 +1,17 @@
 const dummy = () => {
   return true;
 };
+
 function disableMouseEvent() {
   console.debug('Bypass click event script is start');
   setInterval(() => {
     try {
       unsafeWindow.require(['quizaccess_photo/init']).save_error_event = dummy;
       unsafeWindow.require('quizaccess_photo/init').unfocus = dummy;
-    } catch (e) {}
+    } catch (e) {
+      console.debug(e);
+    }
   }, 100);
 }
+
 export default disableMouseEvent;
